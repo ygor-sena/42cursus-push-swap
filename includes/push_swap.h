3f9531b8bf25f0e9a6dc63c329b39a59f774c911
Ygor Sena <yde-goes@student.42sp.org.br>
2022-10-19 05:21:03 +0200

    fix: fixed unformatted norminette code

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yde-goes <yde-goes@student.42sp.org.br     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/19 05:19:38 by yde-goes          #+#    #+#             */
+/*   Updated: 2022/10/19 05:20:39 by yde-goes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
@@ -10,19 +22,19 @@
 
 typedef struct t_stack
 {
-    long long           nbr;
-    struct t_stack    *next;
-}   t_stack;
+	long long			nbr;
+	struct t_stack		*next;
+}	t_stack;
 
-t_stack     *create_node(long long nbr);
-t_stack     *get_last_nbr(t_stack *lst);
-void	    add_back_nbr(t_stack **lst, t_stack *new);
-void	    stack_cleaning(t_stack **lst);
-int         stack_size(t_stack *lst);
+t_stack		*create_node(long long nbr);
+t_stack		*get_last_nbr(t_stack *lst);
+void		add_back_nbr(t_stack **lst, t_stack *new);
+void		stack_cleaning(t_stack **lst);
+int			stack_size(t_stack *lst);
 
-t_bool      is_valid_arg(int argc, char **argv, t_stack *stack_a);
-t_bool      is_nondigit(char *string);
+t_bool		is_valid_arg(int argc, char **argv, t_stack *stack_a);
+t_bool		is_nondigit(char *string);
 
-void        free_split(char **split);
+void		free_split(char **split);
 
 #endif
